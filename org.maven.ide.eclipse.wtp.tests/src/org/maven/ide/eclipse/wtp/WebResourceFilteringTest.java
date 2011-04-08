c011bf89e3525b2a33be490a95647fbfd6898f42
Fred Bricon <fbricon@gmail.com>
2011-04-09 00:40:43 +0200

    Remove unnecessary junit4 reference -for now-. Also improved web filtering IT

--- org.maven.ide.eclipse.wtp.tests/src/org/maven/ide/eclipse/wtp/WebResourceFilteringTest.java
+++ org.maven.ide.eclipse.wtp.tests/src/org/maven/ide/eclipse/wtp/WebResourceFilteringTest.java
@@ -3,19 +3,22 @@ package org.maven.ide.eclipse.wtp;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.codehaus.plexus.util.FileUtils;
 import org.codehaus.plexus.util.IOUtil;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.junit.Test;
+import org.eclipse.wst.common.componentcore.ComponentCore;
+import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
+import org.eclipse.wst.common.componentcore.resources.IVirtualFolder;
 
 public class WebResourceFilteringTest extends AbstractWTPTestCase {
   
   private static String FILTERED_FOLDER_NAME = "target/m2eclipse-wtp/webresources"; 
   
-  @Test
   public void testMECLIPSE22_webfiltering() throws Exception {
     IProject web = importProject("projects/WebResourceFiltering/webfiltering/pom.xml");
     web.build(IncrementalProjectBuilder.FULL_BUILD, new NullProgressMonitor());
@@ -23,6 +26,8 @@ public class WebResourceFilteringTest extends AbstractWTPTestCase {
     assertNoErrors(web);
     IFolder filteredFolder = web.getFolder(FILTERED_FOLDER_NAME);
     assertTrue("Filtered folder doesn't exist", filteredFolder.exists());
+    
+    //Check all the files are correctly filtered
     IFile indexHtml = filteredFolder.getFile("index.html");
     assertTrue("index.html is missing",indexHtml.exists());
     String index = getAsString(indexHtml);
@@ -41,12 +46,38 @@ public class WebResourceFilteringTest extends AbstractWTPTestCase {
     assertEquals("${custom.version} from webfilter.properties was not interpolated","0.0.1-SNAPSHOT",props.get("app.version"));
 
     IFile webXml = filteredFolder.getFile("WEB-INF/web.xml");
-    assertTrue("WEB-INF/web.xml is missing",indexProp.exists());
+    assertTrue(webXml.getName() +" is missing",webXml.exists());
     String xml = getAsString(webXml);
     assertTrue("${welcome.page} from webfilter.properties was not interpolated", xml.contains("<welcome-file>index.html</welcome-file>"));
 
     IFile ignored = filteredFolder.getFile("ignoreme.txt");
     assertFalse("ignoreme.txt should be excluded",ignored.exists());
+    
+    //Check the filtered folder will be deployed first
+    IVirtualComponent webComponent = ComponentCore.createComponent(web);
+    IVirtualFolder rootwar = webComponent.getRootFolder();
+    IResource[] warResources = rootwar.getUnderlyingResources();
+    assertEquals(2, warResources.length);
+    assertEquals(web.getFolder("/target/m2eclipse-wtp/webresources"), warResources[0]);
+    assertEquals(web.getFolder("/src/main/webapp"), warResources[1]);
+    
+    String newFilter = "welcome.page=default.jsp\ncustom.version=1.0";
+    IFile filterFile = web.getFile("src/main/filters/webfilter.properties");
+    FileUtils.fileWrite(filterFile.getLocation().toOSString(), newFilter);
+    filterFile.refreshLocal(1, new NullProgressMonitor());
+    web.build(IncrementalProjectBuilder.INCREMENTAL_BUILD, new NullProgressMonitor());
+    waitForJobsToComplete();
+    
+    indexProp = filteredFolder.getFile("index.properties");
+    assertTrue("index.properties is missing",indexProp.exists());
+    props = new Properties();
+    ins = indexProp.getContents();
+    try {
+      props.load(ins);  
+    } finally {
+      IOUtil.close(ins);
+    }
+    assertEquals("${custom.version} from webfilter.properties was not updated "+ getAsString(filterFile),"1.0",props.get("app.version"));
 
   }
 }
