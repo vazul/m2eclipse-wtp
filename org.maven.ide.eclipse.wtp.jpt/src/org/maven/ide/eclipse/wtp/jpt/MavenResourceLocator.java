403a2d426aacf5734fe2ddabbe521ed7de6361b3
Fred Bricon <fbricon@gmail.com>
2011-07-27 20:49:57 +0200

    Testing ResourceLocator after Dali's fix (https://bugs.eclipse.org/bugs/show_bug.cgi?id=349175)

--- org.maven.ide.eclipse.wtp.jpt/src/org/maven/ide/eclipse/wtp/jpt/MavenResourceLocator.java
+++ org.maven.ide.eclipse.wtp.jpt/src/org/maven/ide/eclipse/wtp/jpt/MavenResourceLocator.java
@@ -3,27 +3,30 @@ package org.maven.ide.eclipse.wtp.jpt;
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.jpt.common.core.internal.resource.SimpleJavaResourceLocator;
 import org.eclipse.jpt.common.core.resource.ResourceLocator;
 
-public class MavenResourceLocator implements ResourceLocator {
+public class MavenResourceLocator extends SimpleJavaResourceLocator implements ResourceLocator {
 
 	public MavenResourceLocator() {
 		System.err.println("Creating MavenResourceLocator" );
 	}
 	
+	@Override
 	public boolean acceptResourceLocation(IProject project, IContainer container) {
-		System.err.println("acceptResourceLocation " + project );
-		return false;
+		//boolean accepts = super.acceptResourceLocation(project, container);
+		//System.err.println("acceptResourceLocation " + project  + "::" + container + " = " + accepts);
+		return true;
 	}
 
 	public IContainer getDefaultResourceLocation(IProject project) {
 		System.err.println("getDefaultResourceLocation " + project );
-		return null;
+		return project.getFolder("src/main/resources/META-INF/");
 	}
 
 	public IPath getResourcePath(IProject project, IPath runtimePath) {
 		System.err.println("getResourcePath " + project + " : " + runtimePath);
-		return null;
+		return project.getFolder("target/classes/").getFullPath().append(runtimePath);
 	}
 
 	public IPath getRuntimePath(IProject project, IPath resourcePath) {
