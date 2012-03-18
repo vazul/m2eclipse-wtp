3683f4dde330769981d4a5eb06cce5354c980854
Fred Bricon <fbricon@gmail.com>
2012-03-18 11:13:24 +0100

    Update target platform to use m2e 1.1 snapshot
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- quick-build-e35.bat
+++
@@ -1,3 +0,0 @@
-echo Building m2eclipse-wtp (without running tests) using Eclipse 3.5 target platform
-set MAVEN_OPTS=-Xmx512m
-mvn clean install -Dmaven.test.skip=true -Dtarget.platform=m2e-wtp-e35
